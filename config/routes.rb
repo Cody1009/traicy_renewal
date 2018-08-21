aa6264bab62c6c62709fd2c68ce9daa4682d1f8d
Cody1009 <nansiki02@gmail.com>
2018-08-21 20:34:37 +0900

    Finish basic settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'traicy/index'
+  root to: 'traicy#index'
 
-  get 'traicy/article_detail'
+  get 'article_detail', to: 'traicy#article_detail'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
