560939ee42f9953f0c30e543bae8fa12e65dfadb
Cody1009 <nansiki02@gmail.com>
2018-08-31 00:13:55 +0900

    style(article detail): create UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'traicy#index'
 
-  get 'article_detail', to: 'traicy#article_detail'
+  get 'article', to: 'traicy#article_detail'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
